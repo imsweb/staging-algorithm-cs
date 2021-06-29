6efe383454da5eb49cfac03827c29481320eadd0
Chuck May <mayc@imsweb.com>
2021-06-29 09:01:37 -0400

    Added more testing data

--- src/test/java/com/imsweb/staging/cs/CsIntegrationTest.java
+++ src/test/java/com/imsweb/staging/cs/CsIntegrationTest.java
@@ -44,7 +44,9 @@ public class CsIntegrationTest {
 
         Stopwatch stopwatch = Stopwatch.create();
 
-        // get list of schema files
+        // get list of schema files;
+        // NOTE: some of these files are REALLY large so they are not included in the project for now;  all files larger than 2MB (40 of them) are not
+        //       part of the repository and can be found at \\omni\btp\csb\Staging\CS
         List<String> schemaFiles;
         try (BufferedReader buffer = new BufferedReader(
                 new InputStreamReader(Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResourceAsStream("integration/schemas/index.txt")), StandardCharsets.UTF_8))) {
